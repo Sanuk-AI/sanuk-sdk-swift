df94b3aea54067872b2cc752627e690051f568a3
Ye Lin Aung <yelinaung2359@gmail.com>
2024-11-01 02:47:28 +0700

    Initial Release

---
+++ Sources/AutoSDK.xcframework/ios-arm64_x86_64-simulator/AutoSDK.framework/Headers/AutoSDK.h
@@ -0,0 +1,17 @@
+//
+//  AutoSDK.h
+//  AutoSDK
+//
+//  Created by Volodymyr Kovalchuk on 21.06.2023.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoSDK.
+FOUNDATION_EXPORT double AutoSDKVersionNumber;
+
+//! Project version string for AutoSDK.
+FOUNDATION_EXPORT const unsigned char AutoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoSDK/PublicHeader.h>
+
+
